32d11322c68e8d0ea246feedeeb02727874389ff
justmahmoud31 <justmahmoud3152003@gmail.com>
2024-09-09 15:11:56 +0300

    adding stripe payment and webhook 📊

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ import { dbconn } from './database/dbconnection.js';
 import { bootstrap } from './src/modules/bootstrap.js';
 import { AppError } from './src/utils/AppError.js';
 import { globalError } from './src/middlewares/globalError.js';
-import cors from "cors"
+import cors from 'cors';
+import Stripe from 'stripe';
+import bodyParser from 'body-parser';
+import { catchError } from './src/middlewares/catchError.js';
 const app = express();
 const port = process.env.PORT || 3000;
+const stripe = new Stripe(process.env.STRIPE_KEY);
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 bootstrap(app);
-app.get('/', (req, res, next) => {
-  res.json({ "Hiiii": "Hi" })
-})
+app.post('/api/webhook', bodyParser.raw({ type: 'application/json' }),catchError ((req, res)=> { 
+  const sig = req.headers['stripe-signature'];
+  let event=stripe.webhooks.constructEvent(req.body,sig,'whsec_qVcIi8FOWr43NeyDi1Ce0EHEpo2TFzeh');
+  let checkout;
+  if(event.type == "checkout.session.completed"){
+   checkout = event.data.object; 
+  }
+
+  res.json({ received: true,checkout });
+}));
+app.get('/', (req, res) => {
+  res.json({ "Hiiii": "Hi" });
+});
 app.get('*', (req, res, next) => {
   next(new AppError(`Route Not Found : ${req.originalUrl}`, 404));
 });
 app.use(globalError);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
